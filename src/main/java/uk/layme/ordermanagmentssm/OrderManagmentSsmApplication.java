3f8ca9738833517a5ba3bedc188f40ee9b35828d
Huillca, Reynaldo <lymreynaldo@icloud.com>
2024-08-05 06:48:09 -0300

    chore: add initial files

---
+++ src/main/java/uk/layme/ordermanagmentssm/OrderManagmentSsmApplication.java
@@ -0,0 +1,13 @@
+package uk.layme.ordermanagmentssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagmentSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagmentSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/layme/ordermanagmentssm/OrderManagmentSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.layme.ordermanagmentssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagmentSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
